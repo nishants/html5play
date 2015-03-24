6bd2106babbe4697668c60cddfc478872524a3c5
nishant <nishant.singh87@gmail.com>
2015-03-24 23:48:54 +0530

    depress image instead of elevating the canvas

--- tutorials/image-draw/image-draw.js
+++ tutorials/image-draw/image-draw.js
@@ -1,24 +1,35 @@
 var app =  {};
 
-var newCanvas= function(){
-    var id = "canvas";
-    return $(jQuery.parseHTML("<canvas id='" + id + "' class='transparent elevated'></canvas>"));
+var newCanvas= function(id){
+    var canvas = $(jQuery.parseHTML("<canvas id='" + id + "' class='transparent elevated'></canvas>"));
+    $("body").append(canvas);
+    return canvas;
 };
 
-var mask = function($target, $with){
-    $("body").append($with);
+var mask = function($target, $with){    
+    $target.addClass("depressed");
+    $with.addClass("fixed-position");
+
     $with.width($target.width());
     $with.height($target.height());
+    
     var left = $target.position().left;
     var top = $target.position().top;
-    
     $with.css({left: left, top: top});
-    $with.addClass("elevated");
 };
 
-app.process = function(imageElement){
-    var $canvas = newCanvas();
-    mask(imageElement, $canvas);
+app.process = function($imageElement){
+    mask($imageElement, newCanvas("canvas"));
 };
 
 window.drawApp = app;
+
+
+
+
+// image = $("#some-image");
+// canvas = $("#canvas");
+// console.log("image: "+image.position().top + " | canvas: " + canvas.position().top);
+// console.log("image: "+image.position().left + " | canvas: " + canvas.position().left);
+// console.log("image: "+image.width() + " | canvas: " + canvas.width());
+// console.log("image: "+image.height() + " | canvas: " + canvas.height());
