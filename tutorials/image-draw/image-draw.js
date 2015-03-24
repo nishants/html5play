ec0f3b5abe0cadff31c9ea6c7916579a4974d6fb
nishant <nishant.singh87@gmail.com>
2015-03-24 21:32:11 +0530

    making canvas perfect overlay of image

--- tutorials/image-draw/image-draw.js
+++ tutorials/image-draw/image-draw.js
@@ -2,17 +2,18 @@ var app =  {};
 
 var newCanvas= function(){
     var id = "canvas";
-    return $(jQuery.parseHTML("<canvas id='" + id + "' class='transparent'><canvas>"));
+    return $(jQuery.parseHTML("<canvas id='" + id + "' class='transparent elevated'></canvas>"));
 };
 
 var mask = function($target, $with){
-    $target.before($with);
+    $("body").append($with);
     $with.width($target.width());
     $with.height($target.height());
     var left = $target.position().left;
     var top = $target.position().top;
     
     $with.css({left: left, top: top});
+    $with.addClass("elevated");
 };
 
 app.process = function(imageElement){
