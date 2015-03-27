707e15eae4425c181b4d6455246ceb13c49232c7
nishant <nishant.singh87@gmail.com>
2015-03-27 14:28:12 +0530

    adding overlay.js

---
+++ tutorials/image-draw/overlay.js
@@ -0,0 +1,96 @@
+
+/*/*************** Dom Dependency ****************/
+var isCanvas = function(element){
+    return element.is("canvas");
+};
+
+var setPosition = function(element, position){
+    element.css({
+        left:   position.left, 
+        top:    position.top
+    });
+};
+
+var setAttribute = function(element, name, value){
+    element.attr(name, value);
+};
+
+var setWidth = function(element, width){
+    element.width(width);
+};
+
+var setHeight = function(element, height){
+    element.height(height);
+};
+
+var getWidth = function(element){
+    element.width();
+};
+
+var getHeight = function(element){
+    element.height();
+};
+
+var setPositionToFixed = function(element){
+    element.css("position", "fixed");
+};
+
+/*/***************Sheet model/****************/
+var Sheet = function($element){
+    this.element = $element;
+};
+
+Sheet.prototype.relocate = function(toPosition){
+    setPosition(this.element, toPosition);
+};
+
+Sheet.prototype.resize = function(toWidth, toHeight){
+    setHeight(this.element, toHeight);
+    setWidth(this.element , toWidth);
+};
+
+/*************** Canvas Model /****************/
+var CanvasSheet = function($element){
+    Sheet.call(this);
+    this.element = $element;
+};
+
+CanvasSheet.prototype = new Sheet();
+
+CanvasSheet.prototype.resize = function(toWidth, toHeight){
+    setAttribute(this.element, "height" , toHeight);
+    setAttribute(this.element, "width"  , toWidth);    
+};
+
+/*********************************************/
+var newSheetWith = function($element){
+    // A sheet elment must have a fixed position.
+    setPositionToFixed($element);
+
+    if(isCanvas($element)){
+      return new Canvasheet($element);  
+    } 
+    return new Sheet($element);
+};
+
+var Mask = function($target, sheet){
+    this.target = $target;
+    this.sheet  = sheet;
+};
+
+Mask.mask = function(){
+    this.sheet.resize(
+        getWidth(this.target),
+        getHeight(this.target)
+    );
+
+    this.sheet.relocate(
+        getPosition(this.target)
+    );
+};
+
+var mask = function(){};
+
+window.org = {
+    geeksaints: {mask: null}
+};
