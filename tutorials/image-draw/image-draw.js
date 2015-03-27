d40ba2abae7be8782d4c7f8f4e34cddfa5d1920f
nishant <nishant.singh87@gmail.com>
2015-03-27 16:36:13 +0530

    use masking api in image draw

--- tutorials/image-draw/image-draw.js
+++ tutorials/image-draw/image-draw.js
@@ -1,46 +1,29 @@
-var app =  {};
-var org = {};
-org.geeksaints = {};
-org.geeksaints.chiesel = {};
-
 var newCanvas= function(id){
     var canvas = $(jQuery.parseHTML("<canvas id='" + id + "' class='transparent elevated'></canvas>"));
     $("body").append(canvas);
     return canvas;
 };
 
-var mask = function($target, $with){    
-    $with.addClass("fixed-position");
-
-    $with.attr("width", $target.width());
-    $with.attr("height", $target.height());
-    
-    var left = $target.position().left;
-    var top = $target.position().top;
-    $with.css({left: left, top: top});
+var Drawing = function(context){
+    this.context = context;
 };
 
-var Image = function($targetElement){
-    this.canvas = newCanvas("canvas");
-    this.element = $targetElement;
-}; 
-
-Image.prototype.refresh = function(){
-    mask(this.element, this.canvas);
-    this.draw();
-};
-
-Image.prototype.draw = function(){
-    var c = document.getElementById(this.canvas.attr("id"))
-    var context = c.getContext("2d");
+Drawing.prototype.draw = function(){
+    var context = this.context;
     context.beginPath();
     context.moveTo(0,0);
     context.lineTo(300,150);
     context.stroke();
+
 };
 
-org.geeksaints.chiesel.editImage = function($targetElement){
-    var image = new Image($targetElement);
-    image.refresh();
-    return image;
+org.geeksaints.chiesel =  {};
+org.geeksaints.chiesel.editImage = function($image){
+    var maskID = "mask";
+    var mask = org.geeksaints.mask($image, newCanvas(maskID));
+    var drawingContext = document.getElementById(maskID).getContext("2d");
+    var drawing = new Drawing(drawingContext);
+    mask.wear(); 
+    setTimeout(function(){drawing.draw();});
+    return drawing;
 };
