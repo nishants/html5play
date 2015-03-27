329ec32c812e41e8af05f99a83bbd93278843bcf
nishant <nishant.singh87@gmail.com>
2015-03-27 17:40:11 +0530

    adding locaiton form to marker page

--- tutorials/image-draw/marker.js
+++ tutorials/image-draw/marker.js
@@ -9,9 +9,14 @@ var Marker = function($target){
     this.element    = $target;
 }; 
 
-var markImagePositions = function($image){
+// class arg is optional(custom styling for the mask)
+var markImagePositions = function($image, maskStyle){
     var maskID = "marker";
     var markingLayer = newDiv(maskID);
+    
+    if(maskStyle){
+      markingLayer.addClass(maskStyle);  
+    } 
 
     var mask = org.geeksaints.mask($image, markingLayer);
     mask.wear(); 
@@ -22,3 +27,20 @@ var markImagePositions = function($image){
 
 org.geeksaints.marker = markImagePositions;
 
+var assertThat = function(sheetVal, imageVal, propertyName){
+    if(!(sheetVal === imageVal)){
+        console.error(propertyName + ": sheet: "+sheetVal + ", image: "+imageVal);
+    } else {
+        console.log(propertyName + " = " + sheetVal);
+    }
+};
+
+window.test = function(){
+    var sheet = $("#marker");
+    var image  = $("#some-image");
+    assertThat(sheet.height(), image.height(), "height");
+    assertThat(sheet.width(), image.width(), "width");
+    
+    assertThat(sheet.position().left, image.position().left, "left");
+    assertThat(sheet.position().right, image.position().right, "right");
+};
