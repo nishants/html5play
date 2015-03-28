94af21eacaa66e33f08aa12b1d9537ae3722f544
nishant <nishant.singh87@gmail.com>
2015-03-28 15:01:51 +0530

    moving inline code from relative.html to relative.js

--- tutorials/image-draw/relative.js
+++ tutorials/image-draw/relative.js
@@ -1,22 +1,33 @@
 
 
+var locationOf = function($element){
+	return {
+		x:  $element.offset().left, 
+		y: $element.offset().top
+	};
+};
+
+var dimensionOf = function($element){
+	return {
+		height: $element.height(),
+		width: $element.width()
+	};
+};
+
+var relativeToAbsolute = function(point, dimension, location){
+	var x, y;
+
+	x = point.x * dimension.width / 100;
+	y = point.y * dimension.height / 100;
+
+	return {
+		x: x,
+		y: y
+	};
+};
+
+
 window.org.geeksaints.toRelative = function(point, image){
-	//x, y, height, length, top, left
-
-
-	var locationOf = function($element){
-		return {
-			x:  $element.offset().left, 
-			y: $element.offset().top
-		};
-	};
-
-	var dimensionOf = function($element){
-		return {
-			height: $element.height(),
-			width: $element.width()
-		};
-	};
 	var dimension = dimensionOf(image), 
 		location = locationOf(image);
 
@@ -32,5 +43,18 @@ window.org.geeksaints.toRelative = function(point, image){
 	};
 };
 
+window.org.geeksaints.getPointFrom = function(e){
+	return {
+		x: e.clientX,
+		y: e.clientY
+	};
+};
 
-			
+		
+window.org.geeksaints.markOnImage = function(point, $element, drawing){
+	var point = relativeToAbsolute(
+		point, 
+		dimensionOf($element)
+	);
+	drawing.markPoint(point.x, point.y);
+};
